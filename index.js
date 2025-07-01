fdd51b6583071ce805d9a4ffeeb8f63e9fe6bc77
Minhajul Islam <mmminhaj221@gmail.com>
2025-07-01 22:14:37 +0600

    added live link to cors

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
+      "https://event-manager-client-six.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
